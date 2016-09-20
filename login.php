78a69a80c83b4d8e4c0bc80152de819c6b6d64db
stenpoks <stenpoks@greeny.cs.tlu.ee>
2016-09-20 21:41:16 +0300

    Lisasin lahtrid

--- login.php
+++ login.php
@@ -6,9 +6,56 @@
 
 var_dump($_POST);
 
-$signupEmailError = "";
 
-// kas keegi vajutas nuppu ja see on olemas
+$signupUsernameError = "*";
+
+//kas kasutaja sisestas soovitud kasutajanime
+
+if (isset ($_POST[singupUsername])) {
+
+  // on olemas
+  // kas kasutajanimi on tühi
+    if (empty ($_POST["signupUsername"])) {
+
+    //on tühi
+      $signupUsernameError = "Väli on kohustuslik!";
+   }
+ }
+
+
+$signupFirstNameError = "*";
+
+//kas kasutaja sisestas eesnime
+
+if (isset ($_POST[singupFirstName])) {
+
+  // on olemas
+  // kas eesnimi on tühi
+    if (empty ($_POST["signupFirstName"])) {
+
+    //on tühi
+      $signupFirstNameError = "Väli on kohustuslik!";
+   }
+ }
+
+ $signupError = "*";
+
+ //kas kasutaja sisestas perekonnanime
+
+ if (isset ($_POST[singupLastName])) {
+
+   // on olemas
+   // kas perekonnanimi on tühi
+     if (empty ($_POST["signupLastName"])) {
+
+     //on tühi
+       $signupLastNameError = "Väli on kohustuslik!";
+    }
+  }
+
+$signupEmailError = "*";
+
+  // kas kasutaja vajutas nuppu ja see on olemas
 
 if (isset ($_POST["signupEmail"])) {
 
@@ -17,19 +64,40 @@ if (isset ($_POST["signupEmail"])) {
     if (empty ($_POST[signupEmail])) {
 
     //on tyhi
-      $signupEmailError = "V2li on kohustuslik!";
+      $signupEmailError = "Väli on kohustuslik!";
     }
   }
 
-// kas epost on tyhi
+// kas password on tühi:
+
+$signupPasswordError = "*";
+
+// kas kasutaja vajutas nuppu ja see on olemas:
+
+if (isset ($_POST["signupPassword"])) {
+
+    //on olemas
+    //kas e-post on tühi:
+    if (empty ($_POST[signupEmail])) {
+
+      //on tühi
+        $signupEmailError = "Väli on kohustuslik!";
+    }
+  }
+
 
 
  ?>
+
 <!DOCTYPE html>
 <html>
   <head>
       <title>Sisselogimise leht</title>
+
+      <script src='https://www.google.com/recaptcha/api.js'></script>
+
 </head>
+
 <body>
 
 <h1>Logi sisse</h1>
@@ -46,7 +114,7 @@ if (isset ($_POST["signupEmail"])) {
 
       <br> <br>
 
-      <input type+"submit" value="Logi sisse">
+      <input type="submit" value="Logi sisse">
 
 </body>
 </html>
@@ -63,17 +131,39 @@ if (isset ($_POST["signupEmail"])) {
 
     <form method="POST">
 
+      <placeholder>Soovitud kasutajanimi</placeholder><br>
+      <input name = "signupUsername" type = "kasutajanimi"> <?php echo $signupUsernameError; ?>
+
+      <br><br>
+
+      <placeholder>Eesnimi</placeholder><br>
+      <input name = "signupFirstName" type = "eesnimi"> <?php echo $signupFirstNameError; ?>
+
+      <br><br>
+
+      <placeholder>Perekonnanimi</placeholder><br>
+      <input name = "signupLastName" type = "perekonnanimi"> <?php echo $signupLastNameError; ?>
+
+      <br><br>
+
       <placeholder>E-post</placeholder><br>
-      <input name="signupEmail" type="email"> <?php echo $signupEmailError; ?>
+      <input name = "signupEmail" type = "email"> <?php echo $signupEmailError; ?>
 
       <br><br>
 
       <placeholder>Password</placeholder><br>
-      <input name="signupPassword" type="parool">
+      <input name="signupPassword" type="parool"> <?php echo $signupPasswordError; ?>
 
       <br> <br>
 
       <input type="submit" value="Loo kasutaja">
+
+      <br><br>
+
+
+
+      <div class="g-recaptcha" data-sitekey="6Ld0OQcUAAAAAPDJsOJo5KFp3_BLzx9zIpj_-qkN"></div>
+
 
 </body>
 </html>
